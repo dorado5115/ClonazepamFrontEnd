e97da9094e3fc83d7fb29325de6275fecdbb7c61
dorado5115 <doradodx@gmail.com>
2021-04-10 11:55:57 -0500

    sign up

--- clonazepam/src/App.js
+++ clonazepam/src/App.js
@@ -5,18 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="">
+        <text>Sign Up</text>
+          <div>
+            <input type="text" placeholder="Username"/>
+          </div>
+          <div>
+            <input type="text" placeholder="Password"/>
+          </div>
+          <div>
+            <input type="text" placeholder="Confirm Password"/>
+          </div>
+          <div>
+            <button>Create Account</button>
+          </div>
+          <text>You have already <a href="#">an Account</a></text>
+        </form>
       </header>
     </div>
   );
