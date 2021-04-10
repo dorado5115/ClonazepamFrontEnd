89710dd055900cccb25ba5630c5c3b3766c51cb6
dorado5115 <doradodx@gmail.com>
2021-04-10 16:40:48 -0500

    listo

--- clonazepam/src/components/LogIn.jsx
+++ clonazepam/src/components/LogIn.jsx
@@ -1,24 +1,35 @@
+  
 import React, { Component } from 'react';
-import '../index.css';
+import '../App.css';
 
 class LogIn extends Component{
   render(){
     return (
-      <form action="#">
-        <text>LogIn</text>
-          <div>
-            <input type="text" placeholder="Username"/>
-          </div>
-          <div>
-            <input type="text" placeholder="Password"/>
-          </div>
-          <div>
-            <button>Sing Up</button>
-          </div>
-          <text class="createAcount">You don't have an Account?<a href="/signUp"> Create one</a></text>
+    <section className = "logIn">
+      <div class="logIn">
+        <form action="">
+            <div>
+              <img src="img/Sin título-1.png"></img> 
+            </div>
+            <text>LogIn</text>
+            <div>
+              <input class ="userNameLi" type="text" placeholder="Username"/>
+            </div>
+            <div>
+              <input class ="passWordLi" type="text" placeholder="Password"/>
+            </div>
+            <div>
+              <button class = "buttonLi">Sing Up</button>
+            </div>
+            <text class="createAcount">You don't have an Account?<a href="/signUp"> Create one</a></text>
           </form>
-      );
-    }
+        </div>
+        <div class="imgHero">
+          <img src="img/piqsels.com-id-srsqr.jpg" atl="person"></img>
+        </div>
+    </section>
+    );
+  }
 }
 
 export default LogIn;
